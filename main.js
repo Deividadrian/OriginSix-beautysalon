28a56a01e6afbf92107bfadf33362001f5e2d8e3
Deividadrian <73372786+Deividadrian@users.noreply.github.com>
2021-09-19 16:19:49 -0300

    Add contacts section
    Foi adicionado a seção de contatos e a API da biblioteca Scrollreveal para animação dos elementos da pagina a medida a mesma é rolada.

--- main.js
+++ main.js
@@ -43,3 +43,20 @@ const swiper = new Swiper('.swiper', {/*Está criando uma constante swiper, que 
   mousewheel: true, //Move o slider com a rodinha do mouse
   keyboard: true //Move com as setas do teclado
 })
+
+/* ScrollReavel : Mostrar elementos quando der scroll na página*/
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700, // 700 mili segundos
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #abaut .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links
+  `, { interval: 100})
